c59fb278dbcb00d45cfe8362ce3f878c9c1913ce
Caio <caio.villardo@yahoo.com>
2020-06-22 23:23:34 -0300

    prova final!

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,11 +1,19 @@
 import React,{Component} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Img2 from "../../assets/img/Img2.svg"
+import '../../assets/css/img.css'
 
 class NotFound extends Component{
     render(){
         return(
-            <h1>Página não encontrada</h1>
+            <>
+            <img className="robo" src={Img2} alt=""/>
+            <div texto>
+            <h4>Erro 404<br></br><br></br></h4>
+            Ohh no!!!<br></br><br></br>
+            Parece que a página que você estava procurando não foi encontrada
+            </div>
+            </>
         );
     }
 }
